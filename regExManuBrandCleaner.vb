ea3ce0dc253e70497b4ddaa73d55bbae63af85b9
Ian Luria <ian.luria@ascential.com>
2019-08-04 15:37:01 -0600

    Building out RegEx expressions

--- regExManuBrandCleaner.vb
+++ regExManuBrandCleaner.vb
@@ -1,35 +1,55 @@
-Function simpleCellRegex(Myrange As Range) As String
+Function simpleCellRegex(userInput As String) As String
 
     Dim regEx As New RegExp
-    Dim badLetterPattern As String
-    Dim userInput As String
-    Dim stringReplace As String
+    Dim badCharacterPattern As String
+
     Dim aPattern As String
     Dim ePattern As String
     Dim oPattern As String
     Dim iPattern As String
-    Dim UPattern As String
+    Dim uPattern As String
     Dim yPattern As String
-    Dim miscPattern As String
+    Dim abbreviationPattern As String
 
-    userInput = Myrange.Value
-
-    badLetterPattern = "[aaaaeeeeeiiiiooouuuu@!#$%^&*]"
-    aPattern
-    ePattern
-    oPattern
-    iPattern
-    UPattern
-    yPattern
-    miscPattern
+    badCharacterPattern = "[@!#$%^&*]"
+    abbreviationPattern = "[,.]+com|[\s,.]g[.]?m[.]?b[.]?h[.]?|[\s,.]c[.]?o[.]?|[\s,.]l[.]?t[.]?d[.]?|[\s,.]s[.]?p[.]?a[.]?|[\s,.]s[.]?a[.]?|[\s,.]i[.]?n[.]?c[.]?|[\s,.]s[.]?l[.]?|[\s,.]p[.]?v[.]?t[.]?"
+    aPattern = "[ÀÁÂÃÄÅáâãäå]"
+    ePattern = "[ÈÉÊËëêéè]"
+    oPattern = "[ÒÓÔÕÖðòóôõö]"
+    iPattern = "[ÌÍÎÏìíîï]"
+    uPattern = "[ÙÚÛÜùúûü]"
+    otherPattern"[ŸÝýÿÇçŽžÑñŠš]"
+    yPattern = "[ŸÝýÿ]"
+    cPattern = "[Çç]"
+    zPattern = "[Žž]"
+    nPattern = "[Ññ]"
+    sPattern = "[Šš]"
 
     With regEx
         .Global = True
         .MultiLine = True
         .IgnoreCase = False
-        .Pattern = badLetterPattern
+        .Pattern = badCharacterPattern
     End With
 
     If regEx.test(userInput) Then
-        regEx.Pattern = aPattern
+        simpleCellRegex = regEx.Replace(userInput, "")
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Else 
+        simpleCellRegex = "No"
+    End If
+
+End Function    
         
