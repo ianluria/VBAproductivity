f56806d24c71e38bf868ad2f3f1ef1faf8206649
Ian Luria <ian.luria@ascential.com>
2019-08-11 17:43:35 -0600

    Preparing to move Strings to Array type

--- regExManuBrandCleaner.vb
+++ regExManuBrandCleaner.vb
@@ -10,9 +10,11 @@ Function simpleCellRegex(userInput As String) As String
     Dim uPattern As String
     Dim yPattern As String
     Dim abbreviationPattern As String
+    Dim vowelPattern As String
 
     badCharacterPattern = "[@!#$%^&*]"
     abbreviationPattern = "[,.]+com|[\s,.]g[.]?m[.]?b[.]?h[.]?|[\s,.]c[.]?o[.]?|[\s,.]l[.]?t[.]?d[.]?|[\s,.]s[.]?p[.]?a[.]?|[\s,.]s[.]?a[.]?|[\s,.]i[.]?n[.]?c[.]?|[\s,.]s[.]?l[.]?|[\s,.]p[.]?v[.]?t[.]?"
+    vowelPattern = "[ÀÁÂÃÄÅáâãäåÈÉÊËëêéèÒÓÔÕÖðòóôõöÌÍÎÏìíîïÙÚÛÜùúûü]"
     aPattern = "[ÀÁÂÃÄÅáâãäå]"
     ePattern = "[ÈÉÊËëêéè]"
     oPattern = "[ÒÓÔÕÖðòóôõö]"
@@ -25,6 +27,9 @@ Function simpleCellRegex(userInput As String) As String
     nPattern = "[Ññ]"
     sPattern = "[Šš]"
 
+
+
+    
     With regEx
         .Global = True
         .MultiLine = True
@@ -34,22 +39,16 @@ Function simpleCellRegex(userInput As String) As String
 
     If regEx.test(userInput) Then
         simpleCellRegex = regEx.Replace(userInput, "")
-
-
-
-
-
-
-
-
-
-
-
-
-
-    Else 
-        simpleCellRegex = "No"
     End If
+
+    regEx.Pattern = vowelPattern
+
+    If regEx.test(userInput) Then
+        Debug.Print "Vowel detected."
+    End If
+
+
+
 
 End Function    
         
